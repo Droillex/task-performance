0d1f96ee7e4093d8b20dbf4c47b0733c65457c58
679dsy <679dsy@gmail.com>
2022-05-12 21:17:34 +0300

    task-performance

--- src/contacts.js
+++ src/contacts.js
@@ -1,26 +1,41 @@
 const contacts = document.getElementsByClassName("contacts")[0];
+const contactsContainer = document.getElementsByClassName("contacts-container")[0];
 const stickyHeader = document.getElementsByClassName("stickyHeader")[0];
 
-function addContacts() {
-  const fragment = document.createDocumentFragment();
-  for (let i = 0; i < 50000; i++) {
-    const child = document.createElement("div");
-    child.textContent = i;
-    child.classList.add("contact");
-    fragment.appendChild(child);
+const ITEM_HEIGHT = 18.5;
+const CONTAINER_HEIGHT = 300;
+const ITEM_COUNT = 50000;
+const TOLERANCE = 10;
+let config = {from: 0, to: 0};
+
+function rerenderList() {
+  const newConfig = calculateVisibleRange();
+  if (newConfig.from !== config.from || newConfig.to !== config.to) {
+    contactsContainer.innerHTML = "";
+    for(let i = newConfig.from; i < newConfig.to; i++) {
+      const child = document.createElement("div");
+      child.textContent = i;
+      child.classList.add("contact");
+      contactsContainer.appendChild(child);
+    }
+    contactsContainer.style.paddingTop = `${newConfig.topOffset}px`;
+    contactsContainer.style.paddingBottom = `${newConfig.bottomOffset}px`;
   }
-  contacts.appendChild(fragment);
+  config = newConfig;
+}
+
+function calculateVisibleRange() {
+  const from = Math.max(0, Math.floor(contacts.scrollTop / ITEM_HEIGHT) - TOLERANCE);
+  const topOffset = from * ITEM_HEIGHT;
+  const to = Math.min(ITEM_COUNT, Math.ceil(from + (CONTAINER_HEIGHT / ITEM_HEIGHT)) + TOLERANCE);
+  const bottomOffset = (ITEM_COUNT - to) * ITEM_HEIGHT;
+  return {from, to, topOffset, bottomOffset};
 }
 
 contacts.addEventListener("scroll", (e) => {
-  const items = Array.from(contacts.getElementsByClassName("contact"));
-  const itemOffsets = items.map((item) => item.offsetTop);
-  const topItemIndex = itemOffsets.findIndex(
-    (offset) => contacts.scrollTop - offset <= -18
-  );
-  if (topItemIndex !== -1) {
-    stickyHeader.textContent = items[topItemIndex].textContent;
-  }
+  rerenderList();
+  const contactIndex = Math.floor(contacts.scrollTop / ITEM_HEIGHT);
+  stickyHeader.textContent = contactIndex.toString();
 });
 
-addContacts();
+rerenderList();
